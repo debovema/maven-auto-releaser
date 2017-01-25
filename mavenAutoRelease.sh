7f1781092d22406895ca92c078610d0b00355d02
Mathieu Debove <debovema@users.noreply.github.com>
2017-01-25 09:52:54 +0000

    Update mavenAutoRelease.sh

--- mavenAutoRelease.sh
+++ mavenAutoRelease.sh
@@ -1,5 +1,7 @@
 #/bin/sh
 
+MAVEN_AUTO_RELEASER_VERSION=1.0-beta
+
 # the updateReleaseVersionsAndTrigger function will:
 #  1. clone a repository
 #  2. checkout the source branch (master by default)
@@ -13,6 +15,10 @@
 #
 # arguments can only provided by an optional KEY=VALUE file named release.properties in the same directory of this script
 updateReleaseVersionsAndTrigger () {
+  echo "Maven auto releaser v$MAVEN_AUTO_RELEASER_VERSION"
+  echo " https://github.com/debovema/maven-auto-releaser"
+  echo
+
   if [ "$#" -lt 1 ]; then
     echo "${FUNCNAME}(): at least a Git repository URL is required" >&2
     return 1
@@ -28,60 +34,67 @@ updateReleaseVersionsAndTrigger () {
   # use optional release.properties (to retrieve Git user config and to override arguments default values)
   [ ! -f release.properties ] || . ./release.properties
 
+  echo "Arguments:"
   # use arguments if they exist
   if [ "$#" -lt 2 ]; then
-    echo "${FUNCNAME}(): using '$INCREMENT_POLICY' as default increment policy"
+    echo " using '$INCREMENT_POLICY' as default increment policy"
   else
     INCREMENT_POLICY=$2
-    echo "${FUNCNAME}(): using '$INCREMENT_POLICY' as increment policy"
+    echo " using '$INCREMENT_POLICY' as increment policy"
   fi
   if [ "$#" -lt 3 ]; then
-    echo "${FUNCNAME}(): using '$SOURCE_BRANCH' as default source branch"
+    echo " using '$SOURCE_BRANCH' as default source branch"
   else
     SOURCE_BRANCH=$3
-    echo "${FUNCNAME}(): using '$SOURCE_BRANCH' as source branch"
+    echo " using '$SOURCE_BRANCH' as source branch"
   fi
   if [ "$#" -lt 4 ]; then
-    echo "${FUNCNAME}(): using '$RELEASE_TRIGGER_BRANCH' as default release branch"
+    echo " using '$RELEASE_TRIGGER_BRANCH' as default release branch"
   else
     SOURCE_BRANCH=$4
-    echo "${FUNCNAME}(): using '$RELEASE_TRIGGER_BRANCH' as release branch"
+    echo " using '$RELEASE_TRIGGER_BRANCH' as release branch"
   fi
 
   echo
   echo "Releasing $GIT_REPOSITORY_URL, source branch is $SOURCE_BRANCH, release trigger branch is $RELEASE_TRIGGER_BRANCH"
   echo
 
+  echo "== Initialization =="
   # 1. clone the repository to a temporary directory
   TEMP_CLONE_DIRECTORY=$(mktemp -d)
-  echo "Cloning the repository at $GIT_REPOSITORY_URL to $TEMP_CLONE_DIRECTORY"
+  echo "1. Cloning the repository at $GIT_REPOSITORY_URL to $TEMP_CLONE_DIRECTORY"
   git clone -q $GIT_REPOSITORY_URL $TEMP_CLONE_DIRECTORY
   cd $TEMP_CLONE_DIRECTORY
 
   # checkout the release branch
-  echo "Checking out the release branch: $RELEASE_TRIGGER_BRANCH"
-  git checkout $RELEASE_TRIGGER_BRANCH
+  echo " Checking out the release branch: $RELEASE_TRIGGER_BRANCH"
+  git checkout -q $RELEASE_TRIGGER_BRANCH
 
   # use optional release.properties (to retrieve Git user config and to override arguments default values)
   # this is not the same one as previous one but the one from the repository
+  echo " Sourcing release.properties file"
   [ ! -f release.properties ] || . ./release.properties
 
+  echo
+  echo "== Versions update =="
   # 2. checkout the source branch
-  echo "Checking out the source branch: $SOURCE_BRANCH"
+  echo "2. Checking out the source branch: $SOURCE_BRANCH"
   git checkout -q $SOURCE_BRANCH
 
   # 3. retrieve the next versions
-  echo "Retrieving the next versions"
+  echo "3. Retrieving the next versions"
   RELEASE_VERSION=$(mvn -q -N build-helper:parse-version -Dexec.executable="echo" -Dexec.args='${parsedVersion.majorVersion}.${parsedVersion.minorVersion}.${parsedVersion.incrementalVersion}' exec:exec)
   DEV_VERSION=$(mvn -q -N build-helper:parse-version -Dexec.executable="echo" -Dexec.args='${parsedVersion.majorVersion}.${parsedVersion.minorVersion}.${parsedVersion.nextIncrementalVersion}-${parsedVersion.qualifier}' exec:exec)
-  echo "New versions are: RELEASE_VERSION=$RELEASE_VERSION and DEV_VERSION=$DEV_VERSION"
+  echo " New versions are: RELEASE_VERSION=$RELEASE_VERSION and DEV_VERSION=$DEV_VERSION"
 
+  echo
+  echo "== Release triggering =="
   # 4. checkout the release branch
-  echo "Checking out the release branch: $RELEASE_TRIGGER_BRANCH"
-  git checkout $RELEASE_TRIGGER_BRANCH
+  echo "4. Checking out the release branch: $RELEASE_TRIGGER_BRANCH"
+  git checkout -q $RELEASE_TRIGGER_BRANCH
 
   # 5. update the release properties file with new versions
-  echo "Updating the versions in release.properties"
+  echo "5. Updating the versions in release.properties"
   sed -i "s/\(RELEASE_VERSION=\).*\$/\1${RELEASE_VERSION}/" release.properties
   sed -i "s/\(DEV_VERSION=\).*\$/\1${DEV_VERSION}/" release.properties
 
@@ -89,12 +102,21 @@ updateReleaseVersionsAndTrigger () {
   [ -z "$GIT_USER_EMAIL" ] || git config user.email $GIT_USER_EMAIL
 
   # 6. trigger the release by pushing the new file
-  echo "Triggering the release"
-  git add release.properties && git commit -m "Triggering release"
-  git push origin release
+  echo "6. Triggering the release"
+  git add release.properties && git commit -q -m "Triggering release" &> /dev/null
+  COMMIT_RESULT=$?
+  if [ $COMMIT_RESULT -gt 0 ]; then
+    echo "problem with commit"
+  else
+    echo " Push to the release trigger branch";
+    #git push origin release
+  fi
 
   # clean up and restore initial directory
+  echo
+  echo "== Clean up =="
   cd $OLDPWD
+  echo " Removing temporary directory: $TEMP_CLONE_DIRECTORY"
   rm -rf $TEMP_CLONE_DIRECTORY
 
   return 0
