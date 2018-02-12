8637d397f8a5382ae88fafb6d510cb9f31eba8e4
Mathieu Debove <debovemathieu@gmail.com>
2018-02-12 11:39:40 +0100

    Prepare v1.0.0-beta5

--- maven-auto-release.sh
+++ maven-auto-release.sh
@@ -4,7 +4,7 @@
 # released by Mathieu Debove (https://github.com/debovema) under Apache License, Version 2.0
 
 MAVEN_AUTO_RELEASER_VERSION=1.0.0-beta5 # this is the displayed version (in banner)
-MAVEN_AUTO_RELEASER_VERSION_TAG=master #v$MAVEN_AUTO_RELEASER_VERSION # this is the Git tag used to retrieve template files
+MAVEN_AUTO_RELEASER_VERSION_TAG=v$MAVEN_AUTO_RELEASER_VERSION #v$MAVEN_AUTO_RELEASER_VERSION # this is the Git tag used to retrieve template files
 
 DEFAULT_RELEASE_TRIGGER_BRANCH=release-trigger
 DEFAULT_SOURCE_BRANCH=master
