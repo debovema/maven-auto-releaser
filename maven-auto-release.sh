d08d9710b4068c6a4aa22c04daf1f4c6e6fe3264
Mathieu Debove <debovemathieu@gmail.com>
2018-02-12 18:28:11 +0100

    Removed debug switch in unleash build

--- maven-auto-release.sh
+++ maven-auto-release.sh
@@ -533,7 +533,7 @@ executeRelease () {
   case "$MAVEN_RELEASER" in
     "unleash")
       echo " Executing release build using unleash-maven-plugin releaser."
-      mvn unleash:perform -X -Dunleash.developmentVersion=$DEV_VERSION -Dunleash.releaseVersion=$RELEASE_VERSION
+      mvn unleash:perform -Dunleash.developmentVersion=$DEV_VERSION -Dunleash.releaseVersion=$RELEASE_VERSION
     ;;
     "maven")
       echo " Executing release build using unleash-maven-plugin releaser."
