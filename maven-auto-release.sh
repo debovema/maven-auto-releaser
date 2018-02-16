e882fed68141294c446b08a8fc27d6e6f6784b7e
Mathieu Debove <debovemathieu@gmail.com>
2018-02-16 19:46:40 +0100

    Debug

--- maven-auto-release.sh
+++ maven-auto-release.sh
@@ -560,7 +560,7 @@ executeRelease () {
   case "$MAVEN_RELEASER" in
     "unleash")
       echo " Executing release build using unleash-maven-plugin releaser."
-      echo "$UNLEASH_WORKFLOW_URL"
+
       if [ "$UNLEASH_WORKFLOW_URL" != "" ]; then
         curl -fsSL $UNLEASH_WORKFLOW_URL -o /tmp/unleash-workflow
         WORKFLOW_PARAM="-Dworkflow=/tmp/unleash-workflow"
